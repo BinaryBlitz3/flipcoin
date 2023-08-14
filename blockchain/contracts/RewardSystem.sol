1ec1b1ae4c2a1820b0285a45c0c98df5f8ba6a84
Yash Rajoria <yashkhatri826@gmail.com>
2023-08-14 15:44:36 +0530

    contract modified

--- blockchain/contracts/RewardSystem.sol
+++ blockchain/contracts/RewardSystem.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol"; // For signature verification
+
 import "./Achievement.sol";
 
 contract RewardSystem {
+    using ECDSA for bytes32;
     address public owner;
     mapping(address => uint256) public userBalances;
     Achievement[] public achievements;
@@ -32,32 +36,34 @@ contract RewardSystem {
     }
 
     function claimReward(
+        address user,
         uint256 rewardAmount,
         uint256 nonce,
         bytes memory signature
     ) external {
+        require(user != address(0), "Invalid user address");
         require(rewardAmount > 0, "Reward amount must be greater than 0");
 
-        // Calculate the message hash
         bytes32 messageHash = keccak256(
-            abi.encodePacked(msg.sender, rewardAmount, nonce, address(this))
+            abi.encodePacked(user, rewardAmount, nonce, address(this))
         );
 
-        // Verify the signature
         require(
-            verifySignature(msg.sender, messageHash, signature),
+            verifySignature(user, messageHash, signature),
             "Invalid signature"
         );
 
-        // Perform the reward claim
-        require(
-            token.balanceOf(address(this)) >= rewardAmount,
-            "Insufficient contract balance"
-        );
-        token.transfer(msg.sender, rewardAmount);
+        userBalances[user] += rewardAmount;
+        // payable(user).transfer(rewardAmount);
+        (bool callSuccess, ) = payable(user).call{value: rewardAmount}("");
+        require(callSuccess, "Call failed");
 
-        emit RewardClaimed(msg.sender, rewardAmount, nonce);
+        emit RewardClaimed(user, rewardAmount, nonce);
+        emit RewardCredited(user, rewardAmount);
     }
+
+    // add function for handling batch transactions
+    function handleBatch() public {}
 
     function verifySignature(
         address signer,
@@ -74,4 +80,5 @@ contract RewardSystem {
         uint256 rewardAmount,
         uint256 nonce
     );
+    event RewardCredited(address indexed user, uint256 rewardAmount);
 }
